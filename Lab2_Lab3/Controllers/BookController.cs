4d1af5dec3cf3faf97b904a20bb534205d6c5bec
Admin <Admin@DESKTOP-EQ69H0C>
2021-06-24 10:15:09 +0700

    Add project files.

---
+++ Lab2_Lab3/Controllers/BookController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab2_Lab3.Models;
+
+namespace Lab2_Lab3.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public string HelloTeacher(string university)
+        {
+            return "Hello !" + university;
+        }
+
+        public ActionResult ListBook()
+        {
+            var books = new List<string>();
+            books.Add("HTML5 & CSS3 The complete Manual - Author Name Book 1");
+            books.Add("HTML & CSS Responsive web Design cookbook - Author Name Book 2");
+            books.Add("Professional ASP.NET MVC5 - Author Name Book 3");
+            ViewBag.Books = books;
+            return View();
+        }
+
+        public ActionResult ListBookModel()
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1,"HTML5 & CSS3 The complete " , "Author Name Book 1","Content/Images/Book1.jpg"));
+            books.Add(new Book(2,"HTML & CSS Responsive web Design cookbook","Author Name Book 2", "Content/Images/Book2.jpg"));
+            books.Add(new Book(3,"Professional ASP.NET MVC5" , "Author Name Book 3", "Content/Images/Book3.jpg"));
+            return View(books);
+        }
+
+        public ActionResult EditBook(int id)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "HTML5 & CSS3 The complete ", "Author Name Book 1", "Content/Images/Book1.jpg"));
+            books.Add(new Book(2, "HTML & CSS Responsive web Design cookbook", "Author Name Book 2", "Content/Images/Book2.jpg"));
+            books.Add(new Book(3, "Professional ASP.NET MVC5", "Author Name Book 3", "Content/Images/Book3.jpg"));
+
+            Book book = new Book();
+            foreach (Book b in books)
+            {
+                if (b.Id == id)
+                {
+                    book = b;
+                    break;
+                }
+            }
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+        [HttpPost, ActionName("EditBook")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditBook(int id, string Title, string Author, string ImageCover )
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "HTML5 & CSS3 The complete ", "Author Name Book 1", "Content/Images/Book1.jpg"));
+            books.Add(new Book(2, "HTML & CSS Responsive web Design cookbook", "Author Name Book 2", "Content/Images/Book2.jpg"));
+            books.Add(new Book(3, "Professional ASP.NET MVC5", "Author Name Book 3", "Content/Images/Book3.jpg"));
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            foreach (Book b in books)
+            {
+                if (b.Id == id)
+                {
+                    b.Title = Title;
+                    b.Author = Author;
+                    b.ImageCover = ImageCover;
+                    break;
+                }
+            }
+            return View("ListBookModel", books);
+        }
+
+        public ActionResult CreateBook()
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("CreateBook")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact([Bind(Include = "id , Title ,Author , ImageCover")] Book book)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "HTLM5 & CSS3 The complete Manual", "Author Name Book 1", "/Content/images/Book1.jpg"));
+            books.Add(new Book(2, "HTLM5 & CSS3 Responsive web Design cookbook ", " Author Name Book 2", "/Content/images/Book2.jpg"));
+            books.Add(new Book(3, "Professional APS.NET  MVC5 ", " Author Name Book 3", "/Content/images/Book3.jpg"));
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    
+                    books.Add(book);
+                }
+            }
+            catch (RetryLimitExceededException)
+            {
+                ModelState.AddModelError("", "Error Save Data");
+            }
+            
+            return View("ListBookModel", books);
+        }
+    }
+}
---
+++ Lab2_Lab3/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lab2_Lab3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab2_Lab3")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9071a28f-4d2e-421c-92b8-e49bdf637da1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
